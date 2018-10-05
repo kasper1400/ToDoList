d7111c31d20c0c2798a62e5ecede9f18232b05d3
Kasper <kasper1400@student.hamk.fi>
2018-10-05 18:54:23 +0300

    Add project files.

---
+++ ToDooList/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ToDooList
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://todoolist.azurewebsites.net";
+	}
+}
+
